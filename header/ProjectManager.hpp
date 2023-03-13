1530c575db04ea2aed5754ca641b05fad6cbb838
EthanL0224 <122485666+EthanL0224@users.noreply.github.com>
2023-03-12 21:27:46 -0700

    Update ProjectManager.hpp

--- header/ProjectManager.hpp
+++ header/ProjectManager.hpp
@@ -10,15 +10,11 @@ using namespace std;
 class ProjectManager {
     private:
         vector<Project*> projects;
-        vector<string> names;
-        vector<string> descriptions;
 
     public:
         ProjectManager();
         ~ProjectManager();
         void addProject(string, string);
-        void editProject_Description(string, string);
-        void editProjectName(string);
         void deleteProject(string);
         void outputProjects();
         int ProjectSize();
